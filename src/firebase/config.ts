68821f51846331da7c795836302b2d7925ab2f99
baha didouni <bahadidouni29@gmail.com>
2025-11-04 11:24:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1266400829-ca2ef",
+  "appId": "1:519740566487:web:0d9930933ebde77933b992",
+  "apiKey": "AIzaSyBNmnchtnLg1FwHlOfT3g3BSLu2eAK2CRE",
+  "authDomain": "studio-1266400829-ca2ef.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "519740566487"
+};
